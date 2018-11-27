a9fa7ec31ef833be5bc5cd610169bdb8ff98776e
Quentin GOURDON <quentin.gourdon@gmail.com>
2018-11-27 19:32:20 +0100

    on work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_161329) do
+ActiveRecord::Schema.define(version: 2018_11_27_173253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_161329) do
     t.date "date_of_birth"
     t.string "phone_number"
     t.string "last_name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
