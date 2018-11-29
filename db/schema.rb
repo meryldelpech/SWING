c553b244ef36833d6185a4216352ba3293e5679a
alexandre willot <alexandre.willot@gmail.com>
2018-11-29 11:26:32 +0100

    committing to get latest version

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_173253) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "infos"
     t.string "photo"
+    t.text "infos"
     t.index ["user_id"], name: "index_talents_on_user_id"
   end
 
